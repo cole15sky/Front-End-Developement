8d5c44abb147ac28bce85e43703dc1b3749881ed
sky___ <sky___@Aakashs-MacBook-Pro.local>
2024-06-21 10:27:12 +0545

    Expression

---
+++ JavaScript/Basics_js/8.Expressions.js
@@ -0,0 +1,18 @@
+/* ----------Expressions :-----------------*/
+
+// // Associativity is from left to right
+// concatenate integer with string
+// let ans = 16 + "text"
+// console.log(ans)
+
+// // javascript doesn't take data sensitivly
+// // first 2 integers are added
+// // Then integers and string are concatenated
+// // then string and integer are concatenated
+// let ans1 = 10+ 12 + "text"+2 +3
+// // To set priority to any expression
+// // use parseInt() or use parenthesis
+// console.log(ans1)
+
+// let ans2 =  "text"+2
+// console.log(ans2)
