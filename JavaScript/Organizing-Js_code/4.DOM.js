9b31d16419a18f720f93ed3cd3f5016100b88e09
Sky Blue <dawadiaakash@gmail.com>
2024-07-09 16:00:45 +0545

    Basic DOM

---
+++ JavaScript/Organizing-Js_code/4.DOM.js
@@ -0,0 +1,23 @@
+//Doucument Obkject Model i.e:DOM is an application programming interface (API) for manipulating HTML documents.
+//DOM provides functions that allows us to add, remove and modify parts of the document effectively.
+//It represents an HTML documents as a tree of nodes.
+
+
+// <!DOCTYPE html>
+// <html lang="en">
+// <head>
+//     
+//     <title>Node in DOM </title>
+// </head>
+// <body>
+//     <div class="container"> sky is here </div>
+// </body>
+// </html>
+
+// ---------------------What node means in DOM -------------
+// element type node = <head> <body> <title>
+// (Node in DOM ,sky is here) are the text node.
+
+//Any nodes has relationship to other nodes in the DOM tree,and it is same as described in the traditional family tree.i.e in above code; <body> is the child node for <html> or <html> is the parent nodes for <body>.
+
+
