3a2903187728d3f411b04217617602c52c2ecaa7
Sky Blue <dawadiaakash@gmail.com>
2024-07-14 12:40:02 +0545

    Descrp:

--- JavaScript/Organizing-Js_code/5.Array_reduce.js
+++ JavaScript/Organizing-Js_code/5.Array_reduce.js
@@ -3,6 +3,18 @@ const items =[
     { name:"Chicken",price: 20},
     { name:"Pork",price: 100}, 
 ]
+// The reduce() method executes a reducer function for array element 
+//The reduce () method returns a single value: the fucntion's accumulated result.
+// The reduce () method does not exdcute the function for empty array elements.
+// THe rediuce() method doesnot change the orignal array.
+
+
+
+// =========================NOTE========================
+//At the first callback, there is no return value from the previous callback.\
+//Normally, array element 0 is used as initial value, and the iteration starts from array element 1.\
+//If an initial value is supplied, this is used, and the iteration starts from array element 0.
+
 
 // let totalPrice =0;
 // items.foreach(items =>{
