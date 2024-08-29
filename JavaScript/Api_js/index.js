95423d8db6a8da048a0a69efd8f4eb2b5640c033
Sky Blue <dawadiaakash@gmail.com>
2024-08-29 22:50:44 +0545

    Fetch: stringify

--- JavaScript/Api_js/index.js
+++ JavaScript/Api_js/index.js
@@ -1,13 +1,27 @@
-fetch('https://fakestoreapi.com/products')
-.then(res => {
-    if(res.ok) {
-        console.log('SUCCESS')
-    }
-        else{
-            console.log('NOT SUCCESS')
-
-        }
+fetch('https://fakestoreapi.com/products',{
+    method:'POST',
+    headers:{
+     'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+        name:'User 1'
     })
+})
+.then(res => {
+
+
+    // if(res.ok) {
+    //     console.log('SUCCESS')
+    // }
+    //     else{
+    //         console.log('NOT SUCCESS')
+
+    //     }
+    // 
+    
+
+return res.json()
+})
     
     
 .then(data => console.log(data))
